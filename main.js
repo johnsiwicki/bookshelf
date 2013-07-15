1518a70e4aa156686641e2c0cc30b561fd89a30d
John Siwicki <johnsiwicki@gmail.com>
2013-07-14 22:00:18 -0400

    Files for Chrome App

--- main.js
+++ main.js
@@ -4,11 +4,10 @@ $( "#books" ).sortable();
 $( "#books" ).disableSelection();
  });
 
-$( document ).ready(function() {
-    console.log( "ready!" );
+ 
     $('#books').on('click', '.book-remove', function(e){
     $(this).parent().remove();
-});
+
 });
 // Set the api variable
 var googleAPI = "https://www.googleapis.com/books/v1/volumes?q=harry+potter";
@@ -20,13 +19,13 @@ $.getJSON(googleAPI, function (response) {
     console.log("JSON Data: " + response.items);
 
     // Loop through all the items one-by-one
-    for (var i = 0; i < response.items.length; i++) {
+    for (var i = 6; i < response.items.length; i++) {
 
         // set the item from the response object
         var item = response.items[i];
 
         // Set the book title in the div
-        document.getElementById("books").innerHTML += "<li class=\"fullbook book\" ><a class=\"hastip\" title=\"" + item.volumeInfo.title + "\n"+ item.volumeInfo.authors +"\"><img src=\"" + item.volumeInfo.imageLinks.thumbnail + "\" alt=\"" + item.volumeInfo.title + "\" ></a><span class=\"book-remove\">X</span></li>"; 
+        document.getElementById("books").innerHTML += "<li class=\"fullbook book\" ><a href=\"" + item.volumeInfo.canonicalVolumeLink + "\" class=\"hastip\" title=\"" + item.volumeInfo.title + "\n"+ item.volumeInfo.authors +"\"><img src=\"" + item.volumeInfo.imageLinks.thumbnail + "\" alt=\"" + item.volumeInfo.title + "\" ></a><span class=\"book-remove\">X</span></li>"; 
       }
 });
 
@@ -43,11 +42,14 @@ function search() {
     $.getJSON("https://www.googleapis.com/books/v1/volumes?q=" + document.getElementById('search').value + "",
 
     function gbooks(data) {
+
         console.log(data);
         $.each(data.items, function (i, item) {
-               document.getElementById("books").innerHTML += "<li class=\"fullbook book\" ><a class=\"hastip\" title=\"" + item.volumeInfo.title + "\n"+ item.volumeInfo.authors +"\"><img src=\"" + item.volumeInfo.imageLinks.thumbnail + "\" alt=\"" + item.volumeInfo.title + "\" ></a></li>"; 
-            if (i == 8) return false;
+                 document.getElementById("books").innerHTML += "<li class=\"fullbook book\" ><a class=\"hastip\" title=\"" + item.volumeInfo.title + "\n"+ item.volumeInfo.authors +"\"><img src=\"" + item.volumeInfo.imageLinks.thumbnail + "\" alt=\"" + item.volumeInfo.title + "\" ></a><span class=\"book-remove\">X</span></li>"; 
+            if (i == 0) return false;
         });
+
+
     });
 }
 
@@ -55,5 +57,5 @@ document.getElementById('button').onclick = search;
 
 
 
-
-
+ 
+ 
--- main2.js
+++
@@ -1,9 +0,0 @@
-
-
-
-$( document ).ready(function() {
-    console.log( "ready!" );
-    $('#books').on('click', '.book-remove', function(e){
-    $(this).parent().remove();
-});
-});
